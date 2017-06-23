f8582f0f426cf9b32a1336565174ac0fd1c0b510
farmaker47 <farmaker47@yahoo.cm>
2017-06-23 17:22:02 +0300

    Sigkinisi--Proto farmakeio sto xarti

--- app/src/main/java/com/george/getreadychat/MapsActivity.java
+++ app/src/main/java/com/george/getreadychat/MapsActivity.java
@@ -1,21 +1,27 @@
 package com.george.getreadychat;
 
 import android.content.Intent;
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
+import com.george.getreadychat.data.UserDetails;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private Marker sydneyy, mandress;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,14 +43,55 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng sydney = new LatLng(40.8696, 22.91);
+        sydneyy = mMap.addMarker(new MarkerOptions()
+                .position(sydney)
+                .title("Mandres")
+                .snippet("Farmaker")
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.farmaker)));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 17));
+
+        mMap.setInfoWindowAdapter(new CustomInfoWindowAdapter());
+
+        mMap.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener() {
+            @Override
+            public void onInfoWindowClick(Marker marker) {
+                Intent intent = new Intent(MapsActivity.this,UserToUserMessage.class);
+                startActivity(intent);
+                UserDetails.secondUser = "george soloupis";
+            }
+        });
+    }
+
+    class CustomInfoWindowAdapter implements GoogleMap.InfoWindowAdapter {
+
+        private final View mWindow;
+
+
+        CustomInfoWindowAdapter() {
+            mWindow = getLayoutInflater().inflate(R.layout.custom_info_window, null);
+            /*mContents = getLayoutInflater().inflate(R.layout.custom_info_contents, null);*/
+        }
+
+        @Override
+        public View getInfoWindow(Marker marker) {
+            if (marker.equals(sydneyy)) {
+                return null;
+            }
+            return mWindow;
+        }
+
+        @Override
+        public View getInfoContents(Marker marker) {
+            return null;
+        }
     }
 
     @Override
